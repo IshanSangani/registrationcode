3e8eb990bbdd5aa53e9f8921042fc5a3c058b4ae
Ishan Sangani <116891262+IshanSangani@users.noreply.github.com>
2024-04-28 20:59:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const app = require("./app");
-// const UserModel = require('./models/user.model')
 const db = require('./config/db')
 
-// Use the PORT environment variable if it's available, otherwise default to 3000
+// Use the PORT environment variable provided by Railway, default to 3000 if not available
 const port = process.env.PORT || 3000;
+// Use the HOST environment variable, default to localhost if not available
+const host = process.env.HOST || 'localhost';
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
 
-app.listen(port,()=>{
-    console.log(`Server Listening on Port http://localhost:${port}`);
+app.listen(port, host, ()=>{
+    console.log(`Server Listening on http://${host}:${port}`);
 })
