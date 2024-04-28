c2272fe58aa99e8e315be9a16dfb2d7a0a341d07
Ishan Sangani <116891262+IshanSangani@users.noreply.github.com>
2024-04-28 20:03:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.set('debug', true);
 
 
-const connection = mongoose.createConnection(`mongodb+srv://ishansangani:Ishan123@cluster0.naowgnd.mongodb.net/`).on('open',()=>{console.log("MongoDB Connected");}).on('error',()=>{
+const connection = mongoose.createConnection(`mongodb+srv://ishansangani:Ishan123@cluster0.naowgnd.mongodb.net/lawappusers`).on('open',()=>{console.log("MongoDB Connected");}).on('error',()=>{
     console.log("MongoDB Connection error");
 });
 
