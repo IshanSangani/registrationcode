f56696af089b336acb6c4936cc599bf66b254586
Ishan Sangani <116891262+IshanSangani@users.noreply.github.com>
2024-04-28 20:55:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const app = require("./app");
 // const UserModel = require('./models/user.model')
 const db = require('./config/db')
 
-const port = 3000;
+// Use the PORT environment variable if it's available, otherwise default to 3000
+const port = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
-
 
 app.listen(port,()=>{
     console.log(`Server Listening on Port http://localhost:${port}`);
